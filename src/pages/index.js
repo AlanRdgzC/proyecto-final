49ed48f7d5e5ba18d1ceb639975ceb796bd98387
LuisAdrianCarmona <luisadrian.carmona@hotmail.com>
2023-07-23 18:20:05 -0600

    V1

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import React from "react";
+import Juego from "../components/Juego";
 export default function Home() {
   return (
     <div>
       <h1>home</h1>
+
+      <Juego></Juego>
     </div>
   );
 }
