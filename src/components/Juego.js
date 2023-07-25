275b45517eb54aec8ee776b5885f1b9c630773fe
LuisAdrianCarmona <luisadrian.carmona@hotmail.com>
2023-07-24 21:56:35 -0600

    ccccc

--- src/components/Juego.js
+++ src/components/Juego.js
@@ -6,34 +6,41 @@ import ListGroup from "react-bootstrap/ListGroup";
 
 function Juego() {
   return (
-    <Card style={{ width: "18rem", textAlign: "center" }}>
-      <Card.Img variant="top" src="Images/HK.jpg" />
-      <Card.Body>
-        <Card.Title>Hollow Knight</Card.Title>
-        <Card.Text>
-          ¡Forja tu propio camino en Hollow Knight! Una aventura épica a través
-          de un vasto reino de insectos y héroes que se encuentra en ruinas.
-        </Card.Text>
-      </Card.Body>
-      <ListGroup variant="flush">
-        <ListGroup.Item>
-          FECHA DE LANZAMIENTO:<br></br> 24 FEB 2017
-        </ListGroup.Item>
-        <ListGroup.Item>
-          DESARROLLADOR:<br></br> Team Cherry
-        </ListGroup.Item>
-        <ListGroup.Item>Un jugador</ListGroup.Item>
-      </ListGroup>
-      <Button variant="primary">
-        <NavDropdown title="Añadir a" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/pendientes">Pendientes</NavDropdown.Item>
-          <NavDropdown.Item href="/jugando">Jugando</NavDropdown.Item>
-          <NavDropdown.Item href="/terminados">Terminados</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="/abandonados">Abandonados</NavDropdown.Item>
-        </NavDropdown>
-      </Button>
-    </Card>
+    <>
+      {["Dark"].map((variant) => (
+        <Card style={{ width: "18rem", textAlign: "center" }}>
+          <Card.Img variant="top" src="Images/HK.jpg" />
+          <Card.Body>
+            <Card.Title>Hollow Knight</Card.Title>
+            <Card.Text>
+              ¡Forja tu propio camino en Hollow Knight! Una aventura épica a
+              través de un vasto reino de insectos y héroes que se encuentra en
+              ruinas.
+            </Card.Text>
+          </Card.Body>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              FECHA DE LANZAMIENTO:<br></br> 24 FEB 2017
+            </ListGroup.Item>
+            <ListGroup.Item>
+              DESARROLLADOR:<br></br> Team Cherry
+            </ListGroup.Item>
+            <ListGroup.Item>Un jugador</ListGroup.Item>
+          </ListGroup>
+          <Button variant="primary">
+            <NavDropdown title="Añadir a" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/pendientes">Pendientes</NavDropdown.Item>
+              <NavDropdown.Item href="/jugando">Jugando</NavDropdown.Item>
+              <NavDropdown.Item href="/terminados">Terminados</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/abandonados">
+                Abandonados
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Button>
+        </Card>
+      ))}
+    </>
   );
 }
 
