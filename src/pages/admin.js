fd985776f7723b25b55b36c86a1d9a663b5bff41
AlanRdgzC <A01748185@tec.mx>
2023-07-24 19:00:00 -0600

    admin page

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,7 +1,44 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 function admin() {
-  return <div>Admin</div>;
+  return (
+    <Form>
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Game Name</Form.Label>
+        <Form.Control placeholder="Nombre del juego" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+        <Form.Label>Description</Form.Label>
+        <Form.Control
+          as="textarea"
+          rows={3}
+          placeholder="Descripcion del juego"
+        />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Release Date</Form.Label>
+        <Form.Control placeholder="Fecha de lanzamiento" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>GameDev</Form.Label>
+        <Form.Control placeholder="Desarrolladora del juego" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formGridAddress2">
+        <Form.Label>Players</Form.Label>
+        <Form.Control placeholder="Cantidad de jugadores" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
 }
 
 export default admin;
