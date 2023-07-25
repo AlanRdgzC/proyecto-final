3b07d80c6d028526243fb65c66ecedb9c1562bc6
AlanRdgzC <A01748185@tec.mx>
2023-07-24 19:27:04 -0600

    New Game Card

---
+++ src/components/Juegopeque.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+
+function Juegopeque(props) {
+  return (
+    <Card style={{ width: "12rem", textAlign: "center" }}>
+      <Card.Img variant="top" src={props.image} />
+      <Card.Body>
+        <Card.Title>{props.gametitle}</Card.Title>
+        <DropdownButton id="dropdown-basic-button" title="Estado">
+          <Dropdown.Item href="/jugando">Jugando</Dropdown.Item>
+          <Dropdown.Item href="/pendientes">Pendiente</Dropdown.Item>
+          <Dropdown.Item href="/terminados">Terminado</Dropdown.Item>
+          <Dropdown.Item href="/abandonados">Abandonado</Dropdown.Item>
+        </DropdownButton>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default Juegopeque;
--- src/pages/jugando.js
+++ src/pages/jugando.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Juegopeque from "@/components/Juegopeque";
 
 function jugando() {
   return (
     <div>
       <h1>jugando</h1>
+      <Juegopeque gametitle="Hollow Knight" image="Images/HK.jpg" />
     </div>
   );
 }
