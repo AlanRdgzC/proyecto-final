0b9337366d5eab1e876f7e7e5a926fcbbd058b1d
AlanRdgzC <A01748185@tec.mx>
2023-07-25 13:58:13 -0600

    ab

--- src/pages/abandonados.js
+++ src/pages/abandonados.js
@@ -1,15 +1,9 @@
 import React from "react";
-import Juegopeque from "@/components/Juegopeque";
 
 function abandonados() {
   return (
     <div>
       <h1>abandonados</h1>
-      <Juegopeque
-        gametitle="Hollow Knight"
-        color="danger"
-        image="Images/HK.jpg"
-      />
     </div>
   );
 }
