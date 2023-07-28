d0a1712467a2871531d9453a6e3f9c3ab2a6ed60
LuisAdrianCarmona <luisadrian.carmona@hotmail.com>
2023-07-27 18:56:29 -0600

    csscambios

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+//import axios from "axios";
 import Juego from "../components/Juego";
 import styles from "../styles/index.module.css";
 
